4480dd117777898649f71c9188bd24308477e19f
NielsHavermans <nielshavermans@gmail.com>
2021-05-11 13:58:38 +0200

    update

--- components/CookieControl.vue
+++ components/CookieControl.vue
@@ -7,7 +7,7 @@
             <div>
               <slot name="bar">
                 <h3 v-text="cookies.text.barTitle"/>
-                <p>Om je makkelijker en persoonlijker te helpen, gebruiken wij cookies. Als je op accepteren klikt, geef je aan dat je dat goedvindt. Je kunt ook je <a href="#" @click="cookies.modal = true">voorkeuren aanpassen</a>. Meer weten? <a href="https://www.woonhome.nl/p/cookies/" target="_blank" id="cn-more-info">Bekijk onze cookiepagina</a>.</p>
+                <p>Om je makkelijker en persoonlijker te helpen, gebruiken wij cookies. Als je op accepteren klikt, geef je aan dat je dat goed vindt. Je kunt ook je <a href="#" @click="cookies.modal = true">voorkeuren aanpassen</a>. Meer weten? <a href="https://woonhome.nl/p/cookies/" target="_blank" id="cn-more-info">Bekijk onze cookiepagina</a>.</p>
               </slot>
             </div>
             <div class="cookieControl__BarButtons">
@@ -16,16 +16,15 @@
           </div>
         </div>
       </transition>
-      <button class="cookieControl__ControlButton" aria-label="Cookie control" v-if="cookies.controlButton && colorsSet && cookies.consent" @click="cookies.modal = true">
+      <!--<button class="cookieControl__ControlButton" aria-label="Cookie control" v-if="cookies.controlButton && colorsSet && cookies.consent" @click="cookies.modal = true">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M510.52 255.82c-69.97-.85-126.47-57.69-126.47-127.86-70.17 0-127-56.49-127.86-126.45-27.26-4.14-55.13.3-79.72 12.82l-69.13 35.22a132.221 132.221 0 00-57.79 57.81l-35.1 68.88a132.645 132.645 0 00-12.82 80.95l12.08 76.27a132.521 132.521 0 0037.16 72.96l54.77 54.76a132.036 132.036 0 0072.71 37.06l76.71 12.15c27.51 4.36 55.7-.11 80.53-12.76l69.13-35.21a132.273 132.273 0 0057.79-57.81l35.1-68.88c12.56-24.64 17.01-52.58 12.91-79.91zM176 368c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm32-160c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm160 128c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32z"/></svg>
-      </button>
+      </button>-->
       <transition name="cookieControl__Modal">
         <div class="cookieControl__Modal" v-if="cookies.modal">
           <p v-if="!saved" class="cookieControl__ModalUnsaved" v-text="cookies.text.unsaved"/>
           <div class="cookieControl__ModalContent">
             <div>
               <slot name="modal"/>
-              <button @click="cookies.modal = false" class="cookieControl__ModalClose" v-text="cookies.text.close"/>
               <template v-for="type in ['necessary', 'optional']">
                 <h3 v-text="cookies.text[type]" :key="type.id"/>
                 <ul :key="type.id">
@@ -36,7 +35,7 @@
                         <span v-if="cookie.description" v-html="cookie.description"/>
                       </span>
                       <input v-if="type === 'necessary' && cookie.name !== 'functional'" :id="getCookieFirstName(cookie.name)" type="checkbox" disabled checked/>
-                      <input v-else :id="getCookieFirstName(cookie.name)" type="checkbox" :checked="cookies.enabledList.includes(cookie.identifier || cookies.slugify(getCookieFirstName(cookie.name))) || (cookies.get('cookie_control_consent').length === 0 && cookie.initialState === true)" @change="toogleCookie(cookie)"/>
+                      <input v-else :id="getCookieFirstName(cookie.name)" type="checkbox" v-indeterminate="true" :checked="cookies.enabledList.includes(cookie.identifier || cookies.slugify(getCookieFirstName(cookie.name))) || (cookies.get('cookie_control_consent').length === 0 && cookie.initialState === true)" @change="toogleCookie(cookie)"/>
                       <label :for="getCookieFirstName(cookie.name)" v-html="getName(cookie.name)"/>                      
                     </div>
                   </li>
